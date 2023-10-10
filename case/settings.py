67483da2e86d7fd26584468f97f377ce1d71038b
Rahil-Nelliyali <rahilnelliyali0@gmail.com>
2023-10-10 20:24:51 +0530

    rehosting

--- case/settings.py
+++ case/settings.py
@@ -99,16 +99,16 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'newcases',
-        'USER': 'postgres',
-        'PASSWORD': 'rahil123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'newcases',
+#         'USER': 'postgres',
+#         'PASSWORD': 'rahil123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 import dj_database_url
 
