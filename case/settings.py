21679cf81d3fd90865f52ce62bcb2d96ac31f32e
Rahil-Nelliyali <rahilnelliyali0@gmail.com>
2023-10-10 20:16:45 +0530

    rehosting

--- case/settings.py
+++ case/settings.py
@@ -176,12 +176,10 @@ MEDIAFILES_DIRS = [
 
 # email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_USE_TLS = True
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='projecticases@gmail.com'
+EMAIL_HOST_PASSWORD='dlgfcgfopuzoxjtq'
 
 #twilio configuration
 
