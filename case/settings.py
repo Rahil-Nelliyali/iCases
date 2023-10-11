a236822c7aba218114513f4361e2855bed40e514
Rahil-Nelliyali <rahilnelliyali0@gmail.com>
2023-10-11 11:40:41 +0530

    updations

--- case/settings.py
+++ case/settings.py
@@ -176,12 +176,12 @@ MEDIAFILES_DIRS = [
 
 
 # email configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER='projecticases@gmail.com'
-EMAIL_HOST_PASSWORD='dlgfcgfopuzoxjtq'
+EMAIL_HOST_USER = "drivenowsmtp@gmail.com"
+EMAIL_HOST_PASSWORD = "robgnbcklnasmnje"
 
+EMAIL_USE_TLS = True
 #twilio configuration
 
 OTP_SECRET = ''
